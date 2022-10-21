ddf202ce11cc6f15f7e831f281f4287a14bd511e
DasolKang <dasol199@naver.com>
2022-10-21 13:36:08 +0900

    initialize

---
+++ src/main/java/com/codestates/behomeworktodoapp/BeHomeworkTodoappApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.behomeworktodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeHomeworkTodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeHomeworkTodoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/behomeworktodoapp/BeHomeworkTodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.behomeworktodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeHomeworkTodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
