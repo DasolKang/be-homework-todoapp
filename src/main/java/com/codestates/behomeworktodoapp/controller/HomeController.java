0738add46e3dd776fe3f919e8a055265195c5ef2
DasolKang <dasol199@naver.com>
2022-10-21 13:40:45 +0900

    :sparkles: add home controller

---
+++ src/main/java/com/codestates/behomeworktodoapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.codestates.behomeworktodoapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String Home() {
+        return "To-do Application !";
+    }
+}
